fe1c8a478ff136df0e871eef3cac75d12d702774
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-09 07:28:10 -0800

    contain examples of return type annotation of an object literal

--- objects.ts
+++ objects.ts
@@ -7,3 +7,15 @@ function printName(person: {first: string, last: string}): void {
 }
 
 printName({first: 'John', last: 'Mayer'})
+
+// We can have a function with a return type annotation of an object literal.
+
+// We can have a variable where we use a object type and of course we can write the parameter type annotations
+
+// for a function to be an object as well.
+
+let coordinates: {x:number, y:number} = {x:32, y:33};
+
+function someCoordinates() : {x: number, y:number} {
+    return {x: Math.random(), y: Math.random()}
+}

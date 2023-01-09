a75100dc90bc169e6b5b33ebce01bd01e9f14c84
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-09 13:55:13 -0800

    start lesson on array

---
+++ arrays.ts
@@ -0,0 +1,9 @@
+// Array types
+// Arrays can be typed using a type annotation followed by empty array brackets, like number for an array of numbers 
+
+const age: number[] = []
+age.push(54)
+
+const names: string[] = ["james", "john", "joseph"]
+names.push("jerry")
+

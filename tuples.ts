8ab2873c5cd093c08bbbb8ef87cea7d4770e3766
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-10 10:56:55 -0800

    contain another example of tuples

--- tuples.ts
+++ tuples.ts
@@ -17,3 +17,13 @@ const color: [number, number, number] = [13, 23, 21]
 // But TypeScript adds some extra rules to make sure in this case we have three numbers and only three
 
 // numbers in this array.
+
+// ---Another example
+
+type HTTPResponse = [number, string];
+
+const goodRes: HTTPResponse = [200, "OK"];
+
+//Limitation of tuple --  once the array is created, we can use the push and pop and maybe more methods to add and remove items from the array!
+
+const responses: HTTPResponse[] = [[404, "Not found"], [200, "OK"]];

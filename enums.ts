8e1fc2bb504b19db0a76154cf880abd68c35a52e
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-10 11:08:06 -0800

    contain an example of enums

---
+++ enums.ts
@@ -0,0 +1,21 @@
+// Enums 
+
+// Enums allow us to define a set of names constants. We can give these constant numeric or string values.
+
+// There’s quite a few options when it comes to enums. 
+
+
+enum OrderStatus {
+    PENDING, 
+    SHIPPED, 
+    DELIVERED, 
+    RETURNED
+}
+
+const mystatus = OrderStatus.DELIVERED;
+
+function isDelivered(status: OrderStatus) {
+    return status === OrderStatus.DELIVERED
+}
+
+isDelivered(OrderStatus.RETURNED)

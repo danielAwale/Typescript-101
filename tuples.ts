63a134a5a31b92fec11f2ddbc9745274b3a95b0e
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-10 10:48:51 -0800

    add some more comments

--- tuples.ts
+++ tuples.ts
@@ -7,3 +7,13 @@
 //example
 
 const color: [number, number, number] = [13, 23, 21]
+
+// They are arrays of fixed length and types.
+
+// They don't exist in JavaScript.
+
+// It's just a regular array behind the scenes.
+
+// But TypeScript adds some extra rules to make sure in this case we have three numbers and only three
+
+// numbers in this array.

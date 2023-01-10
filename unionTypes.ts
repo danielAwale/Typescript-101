9e7310d8398ed96239f3ab17266b21c4f52fcb13
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-10 07:25:37 -0800

    contain an example of narrowing the type

--- unionTypes.ts
+++ unionTypes.ts
@@ -13,3 +13,21 @@ type Loc = {
 
 let coordinates: Points | Loc = {x: 21, y:23}
 coordinates = {lat: 321.12, long: 322.1}
+
+// UNIONS - NARROWING THE TYPE
+
+// Narrowing the type is simply doing a typecheck before working with a value. If your value is a spring you may want to use it differently than if you got a number.
+
+// Since unions allow multiple types for a value, its good to check what came through before working with it. 
+
+function printAge (age: number): void {
+    console.log(`I am ${age} years old`);
+}
+
+function calculateTax(price: number | string, tax: number): number {
+    if (typeof price === "string"){
+        price = parseFloat(price.replace("$", ""));   
+    } 
+        return price * tax;
+}
+

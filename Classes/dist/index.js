1ff39825c32885de7d4443159934724fdb019553
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 12:25:48 -0800

    contain an example of private field

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -2,7 +2,7 @@
 console.log("it's working??");
 
 class Player {
-    score = 0; 
+    #score = 0; 
     Numlives = 10;
     constructor(first, last){
         this.first = first;
@@ -14,15 +14,26 @@ class Player {
     loseLives(){
         this.Numlives -= 1
     }
+    getScore() {
+        return this.#score;
+    }
+    setScore(set) {
+        this.#score = set
+    }
 }
 
 const player1 = new Player("blue", "steele")
 // player1.taunt();
 // console.log(player1.first)
 // console.log(player1.last)
-console.log(player1.Numlives);
-player1.loseLives()
-console.log(player1.Numlives)
+// console.log(player1.Numlives);
+// player1.loseLives()
+// console.log(player1.Numlives)
+// player1.#score;
+console.log(player1.getScore())
+player1.setScore(42)
+console.log(player1.getScore())
+
 
 const player2 = new Player("daniel", "awale")
 player2.taunt();

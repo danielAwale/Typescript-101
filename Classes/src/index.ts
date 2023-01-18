b9b347805c2c9bcf1d4d10c30f2205433110d02c
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 14:58:34 -0800

    contain an example of field classes

--- Classes/src/index.ts
+++ Classes/src/index.ts
@@ -1,10 +1,12 @@
 class Player {
     first: string;
     last: string;
+    score: number = 0;
     constructor(first: string, last: string){
         this.first = first
         this.last = last
     }
+
 }
 
 const john = new Player("John", "Watson");

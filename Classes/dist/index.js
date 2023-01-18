8c0239c84a638a2e8621dc42cdb62a2e8615d99e
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 12:35:29 -0800

    contain n example with getter

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -8,9 +8,16 @@ class Player {
         this.first = first;
         this.last = last;
     }
+    get fullName() {
+        return `${this.first} ${this.last}`
+    }
+    get score() {
+        return this.#score
+    }
     taunt(){
         console.log("BOOYAH!")
     }
+
     loseLives(){
         this.Numlives -= 1
     }
@@ -30,11 +37,13 @@ const player1 = new Player("blue", "steele")
 // player1.loseLives()
 // console.log(player1.Numlives)
 // player1.#score;
-console.log(player1.getScore())
-player1.setScore(42)
-console.log(player1.getScore())
+// console.log(player1.getScore())
+// player1.setScore(42)
+// console.log(player1.getScore())
+console.log(player1.fullName);
+console.log(player1.score);
 
 
 const player2 = new Player("daniel", "awale")
-player2.taunt();
+// player2.taunt();
 

4845039a5f0817a93acd5b77fecd60b4e48def45
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 12:10:40 -0800

    contain an example of constructor

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -2,15 +2,21 @@
 console.log("it's working??");
 
 class Player {
+    constructor(first, last){
+        this.first = first;
+        this.last = last;
+
+    }
     taunt(){
         console.log("BOOYAH!")
     }
 }
 
-const player1 = new Player()
+const player1 = new Player("blue", "steele")
 player1.taunt();
-console.log(player1);
+console.log(player1.first)
+console.log(player1.last)
 
-const player2 = new Player()
+const player2 = new Player("daniel", "awale")
 player2.taunt();
 

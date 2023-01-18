3f067404fe3320f261f972c1c941ed09f0ad2a19
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 12:07:00 -0800

    contain an example of classes in javascript

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -1,2 +1,16 @@
 "use strict";
 console.log("it's working??");
+
+class Player {
+    taunt(){
+        console.log("BOOYAH!")
+    }
+}
+
+const player1 = new Player()
+player1.taunt();
+console.log(player1);
+
+const player2 = new Player()
+player2.taunt();
+

351beb5260afb8a84f282cac421c45535c48d279
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 12:16:42 -0800

    contain an example of class fields

--- Classes/dist/index.js
+++ Classes/dist/index.js
@@ -2,20 +2,27 @@
 console.log("it's working??");
 
 class Player {
+    score = 0; 
+    Numlives = 10;
     constructor(first, last){
         this.first = first;
         this.last = last;
-
     }
     taunt(){
         console.log("BOOYAH!")
     }
+    loseLives(){
+        this.Numlives -= 1
+    }
 }
 
 const player1 = new Player("blue", "steele")
-player1.taunt();
-console.log(player1.first)
-console.log(player1.last)
+// player1.taunt();
+// console.log(player1.first)
+// console.log(player1.last)
+console.log(player1.Numlives);
+player1.loseLives()
+console.log(player1.Numlives)
 
 const player2 = new Player("daniel", "awale")
 player2.taunt();

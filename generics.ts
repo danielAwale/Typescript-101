91dfec3ffcbdfd189744f8518a7318798822f929
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-18 19:36:48 -0800

    contain intro to generics

---
+++ generics.ts
@@ -0,0 +1,9 @@
+// GENERICS 
+
+// Generics allow us to define reusable functions and classes that work with multiple types rather than a single type 
+
+// The syntax is not pretty. They are used all over the place, so it is best to get comfortable with them. 
+
+//const nums: number[] = []
+const nums: Array<number> = []
+const colors: Array<string> = []

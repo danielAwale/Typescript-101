5d45b9b1ac75038582f8a2190434f57f4d8c47f6
Daniel Awale <danielmadhukarawale@gmail.com>
2023-01-21 15:18:13 -0800

    work with axios

--- TypeDeclaration/dist/index.js
+++ TypeDeclaration/dist/index.js
@@ -1,1 +1,13 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const axios_1 = __importDefault(require("axios"));
+axios_1.default.get("https://jsonplaceholder.typicode.com/users/1").then(res => {
+    console.log("Woo!");
+    console.log(res.data);
+})
+    .catch(e => {
+    console.log("error");
+});
--- TypeDeclaration/src/index.ts
+++ TypeDeclaration/src/index.ts
@@ -1,1 +1,10 @@
 import axios from "axios";
+axios.get("https://jsonplaceholder.typicode.com/users/1"
+).then(res => {
+    console.log("Woo!")
+    console.log(res.data)
+})
+.catch(e => {
+    console.log("error")
+})
+
